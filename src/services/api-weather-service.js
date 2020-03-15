779fbc29b580ad65602455152f7979062f92930e
Washington Bruno <washingtonbruno1@gmail.com>
2020-03-15 02:03:58 -0300

    Add separate vue resource

---
+++ src/services/api-weather-service.js
@@ -0,0 +1,28 @@
+import Vue from 'vue';
+import VueResource from 'vue-resource';
+
+Vue.use(VueResource);
+
+export async function getWeatherInfo(cityId) {
+    // TODO Create Env file
+    const apiKey = '5c97dec5122c0098153b2e306fe538fa';
+    try {
+        const response = await Vue.http.get(`http://api.openweathermap.org/data/2.5/weather?id=${cityId}&appid=${apiKey}&units=metric`);
+        return cityWeatherDto(response.body);
+    }catch (e) {
+        console.error(e);
+    }
+}
+
+function cityWeatherDto({id, name, sys:{country} , main:{temp, pressure, humidity}}) {
+    const info = {
+        id,
+        name,
+        country,
+        temp,
+        pressure,
+        humidity,
+        updatedAt: new Date()
+    };
+    return info
+}
